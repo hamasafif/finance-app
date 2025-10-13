a1783c167daedc636aa8e0d5c763d03b5e4341a1
wrjunior <wrjunior@ubuntuserver.han-fence.ts.net>
2025-10-13 10:53:23 +0000

    Perbaikan backend

--- backend/server.js
+++ backend/server.js
@@ -1,13 +1,19 @@
 import express from "express";
 import cors from "cors";
 import transactions from "./routes/transactions.js";
-import "./db.js"; // langsung panggil koneksi
+import "./db.js";
 
 const app = express();
-app.use(cors());
-app.use(express.json());
 
+app.use(cors({
+  origin: ["http://localhost:5173", "http://192.168.1.100:5173"],
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+}));
+
+app.use(express.json());
 app.use("/api/transactions", transactions);
 
 const PORT = 5000;
-app.listen(PORT, () => console.log(`✅ Server running on http://localhost:${PORT}`));
+app.listen(PORT, "0.0.0.0", () => 
+  console.log(`✅ Server running at http://192.168.1.100:${PORT}`)
+);
